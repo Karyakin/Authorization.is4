bb6e4087019690f7a5c731af85c0ca7609b5a123
Дмитрий Карякин <375447955552dk@gmail.com>
2022-05-05 16:24:52 +0300

    Init

---
+++ Authorization.Users.Api/Controllers/SiteController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authorization.Users.Api.Controllers
+{
+    public class SiteController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetOrders()
+        {
+            ViewBag.Massage = "Test";
+            return View();
+        }
+    }
+}
